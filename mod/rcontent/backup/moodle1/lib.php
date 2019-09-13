projectestac/agora_moodle2

Marsupial: Installation

mod/rcontent/backup/moodle1/lib.php
@@ -1,0 +1,188 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Provides support for the conversion of moodle1 backup to the moodle2 format
+ *
+ * @package    mod
+ * @subpackage rcontent
+ * @copyright  2012 Antoni Bertran <abertranb>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Rcontent conversion handler
+ */
+class moodle1_mod_rcontent_handler extends moodle1_mod_handler {
+
+    /** @var moodle1_file_manager */
+    protected $fileman = null;
+
+    /** @var int cmid */
+    protected $moduleid = null;
+
+    /**
+     * Declare the paths in moodle.xml we are able to convert
+     *
+     * The method returns list of {@link convert_path} instances.
+     * For each path returned, the corresponding conversion method must be
+     * defined.
+     *
+     * Note that the path /MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT does not
+     * actually exist in the file. The last element with the module name was
+     * appended by the moodle1_converter class.
+     *
+     * @return array of {@link convert_path} instances
+     */
+    public function get_paths() {
+        return array(
+            new convert_path('rcontent', '/MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT',
+                array(
+                    'renamefields' => array(
+                        'bookid' => 'isbn'
+                    )
+                )
+            ),
+            new convert_path('rcontent_grades', '/MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT/GRADES/GRADE'),
+            new convert_path('rcontent_grades_details', '/MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT/GRADES_DETAILS/GRADE_DETAIL',
+                array(
+                    'renamefields' => array(
+                        'detailid' => 'code',
+                        'detailtypeid' => 'typeid',
+                    )
+                )
+            )
+        );
+    }
+
+    /**
+     * This is executed every time we have one /MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT
+     * data available
+     */
+    public function process_rcontent($data) {
+        global $CFG;
+        // get the course module id and context id
+        $instanceid     = $data['id'];
+        $currentcminfo  = $this->get_cminfo($instanceid);
+        $this->moduleid = $currentcminfo['id'];
+        $contextid      = $this->converter->get_contextid(CONTEXT_MODULE, $this->moduleid);
+
+        // conditionally migrate to html format in summary
+        if ($CFG->texteditors !== 'textarea') {
+            $data['summary']       = text_to_html($data['summary'], false, false, true);
+        }
+
+        // get a fresh new file manager for this instance
+        $this->fileman = $this->converter->get_file_manager($contextid, 'mod_rcontent');
+
+        // convert course files embedded into the summary
+        $this->fileman->filearea = 'summary';
+        $this->fileman->itemid   = 0;
+        $data['summary'] = moodle1_converter::migrate_referenced_files($data['summary'], $this->fileman);
+
+        // check 1.9 version where backup was created
+        $backupinfo = $this->converter->get_stash('backup_info');
+        if ($backupinfo['moodle_version'] < 2007110503) {
+            // as we have no module version data, assume $currmodule->version <= $module->version
+            // - fix data as the source 1.9 build hadn't yet at time of backing up.
+            if (isset($data['grademethod']))
+                $data['grademethod'] = $data['grademethod']%10;
+        }
+
+        // start writing rcontent.xml
+        $this->open_xml_writer("activities/rcontent_{$this->moduleid}/rcontent.xml");
+        $this->xmlwriter->begin_tag('activity', array('id' => $instanceid, 'moduleid' => $this->moduleid,
+            'modulename' => 'rcontent', 'contextid' => $contextid));
+        $this->xmlwriter->begin_tag('rcontent', array('id' => $instanceid));
+
+        foreach ($data as $field => $value) {
+            /*if ($field == 'intro') {
+                //changed from intro to summary
+                $this->xmlwriter->full_tag('summary', $value);
+
+            }
+            else*/if ($field <> 'id') {
+                $this->xmlwriter->full_tag($field, $value);
+            }
+        }
+        
+        $this->xmlwriter->begin_tag('grades');
+        
+        return $data;
+    }
+    
+    
+    /**
+     * This is executed when finish grades
+     * data available
+     */
+    /*public function on_rcontent_grades_end() {
+        $this->xmlwriter->begin_tag('grades_details');
+    }*/
+
+    /**
+     * This is executed every time we have one /MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT/GRADES/GRADE
+     * data available
+     */
+    public function process_rcontent_grades($data) {
+        $this->write_xml('grade', $data, array('/grade/id'));
+    }
+    
+    /**
+    * This is executed when the parser reaches the <GRADES_DETAILS> opening element
+    */
+    public function on_rcontent_grades_details_start() {
+        $this->xmlwriter->begin_tag('grades_details');
+    }
+    
+    /**
+     * This is executed when the parser reaches the closing </GRADES_DETAILS> element
+     */
+    public function on_rcontent_grades_details_end() {
+        $this->xmlwriter->end_tag('grades_details');
+    }
+    /**
+     * This is executed every time we have one /MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT/GRADES/GRADE
+     * data available
+    */
+    public function process_rcontent_grades_details($data) {
+        $this->write_xml('grade_detail', $data, array('/grade_detail/id'));
+    }
+    
+    /**
+     * This is executed when we reach the closing </MOD> tag of our 'rcontent' path
+     */
+    public function on_rcontent_end() {
+        // close rcontent.xml
+        $this->xmlwriter->end_tag('grades'); //Changed structure from rcommon->grade and  rcommon->grades_details to  rcommon->grade->grades_details
+        $this->xmlwriter->end_tag('rcontent');
+        $this->xmlwriter->end_tag('activity');
+        $this->close_xml_writer();
+
+        // write inforef.xml
+        $this->open_xml_writer("activities/rcontent_{$this->moduleid}/inforef.xml");
+        $this->xmlwriter->begin_tag('inforef');
+        $this->xmlwriter->begin_tag('fileref');
+        foreach ($this->fileman->get_fileids() as $fileid) {
+            $this->write_xml('file', array('id' => $fileid));
+        }
+        $this->xmlwriter->end_tag('fileref');
+        $this->xmlwriter->end_tag('inforef');
+        $this->close_xml_writer();
+    }
+}

mod/rcontent/backup/moodle2/backup_rcontent_stepslib.php
@@ -1,0 +1,126 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package moodlecore
+ * @subpackage backup-moodle2
+ * @copyright 2010 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Define all the backup steps that will be used by the backup_rcontent_activity_task
+ */
+
+/**
+ * Define the complete rcontent structure for backup, with file and id annotations
+ */
+class backup_rcontent_activity_structure_step extends backup_activity_structure_step {
+
+    protected function define_structure() {
+
+        // To know if we are including userinfo
+        $userinfo = $this->get_setting_value('userinfo');
+
+        // Define each element separated
+        $rcontent = new backup_nested_element('rcontent', array('id'), array(
+            'course', 'name', 'summary', 'levelid',
+            /*'bookid', 'unitid', 'activityid',*/ 'whatgrade',
+            'popup', 'popup_options', 'frame', 'width',
+            'height', 'timecreated', 'timemodified', 'levelcode', 'isbn', 'unitcode', 'activitycode'));
+
+        $grades = new backup_nested_element('grades');
+
+        $grade = new backup_nested_element('grade', array('id'), array(
+            'userid', 'rcontentid', /*'unitid', 'activityid',*/
+            'grade', 'mingrade', 'maxgrade', 'attempt', 'maxattempts',
+            'starttime', 'totaltime', 'maxtotaltime', 'status',
+            'comments', 'urlviewresults', 'sumweights', 
+            'timecreated', 'timemodified', 'unitcode', 'activitycode'));
+
+        $grades_details = new backup_nested_element('grades_details');
+
+        $grade_detail = new backup_nested_element('grade_detail', array('id'), array(
+            'userid', 'rcontentid', /*'unitid', 'activityid',*/ 'code',
+            'typeid', 'description', 'grade', 'mingrade', 'maxgrade',
+            'starttime', 'totaltime', 'maxtotaltime', 'attempt', 'maxattempts',
+            'weight', 'urlviewresults', 'timecreated', 'timemodified', 'unitcode', 'activitycode'));
+
+        $track_credentials = new backup_nested_element('track_credentials');
+
+        $track_credential = new backup_nested_element('track_credential', array('id'), array(
+            'username', 'password', 'publisherid', 'timecreated', 'timemodified'));
+
+        // Build the tree
+        $rcontent->add_child($grades);
+        $grades->add_child($grade);
+
+        $grade->add_child($grades_details);
+        $grades_details->add_child($grade_detail);
+
+        $rcontent->add_child($track_credentials);
+        $track_credentials->add_child($track_credential);
+
+        // Define sources
+        //$rcontent->set_source_table('rcontent', array('id' => backup::VAR_ACTIVITYID));
+        $rcontent->set_source_sql('SELECT rc.*,rlevel.code as levelcode, rcb.isbn as isbn, unit.code as unitcode, activity.code as activitycode
+              FROM {rcontent} rc
+              LEFT outer JOIN {rcommon_level} rlevel on rlevel.id=rc.levelid
+              LEFT outer JOIN {rcommon_books} rcb on rcb.id=rc.bookid and rcb.levelid=rc.levelid
+              LEFT outer JOIN {rcommon_books_units} unit on unit.id=rc.unitid and unit.bookid=rc.bookid 
+              LEFT outer JOIN {rcommon_books_activities} activity on activity.id=rc.activityid and activity.bookid=rc.bookid and activity.unitid = rc.unitid
+             WHERE rc.id = ?', array(backup::VAR_ACTIVITYID));
+
+        // Use set_source_sql for other calls as set_source_table returns records in reverse order
+        // and order is important for several rcontent fields - esp rcontent_scoes.
+        $grade->set_source_sql('
+                SELECT rg.*, unit.code as unitcode, activity.code as activitycode
+                FROM {rcontent_grades} rg
+                INNER JOIN {rcontent} rc ON rc.id=rg.rcontentid
+                LEFT outer JOIN {rcommon_books_units} unit on unit.id=rc.unitid and unit.bookid=rc.bookid 
+                LEFT outer JOIN {rcommon_books_activities} activity on activity.id=rc.activityid and activity.bookid=rc.bookid and activity.unitid = rc.unitid
+                WHERE rg.rcontentid = :rcontent
+                ORDER BY rg.id',
+            array('rcontent' => backup::VAR_PARENTID));
+
+        $grade_detail->set_source_sql('
+                SELECT rc.*, unit.code as unitcode, activity.code as activitycode
+                FROM {rcontent_grades_details} rg
+                INNER JOIN {rcontent} rc ON rc.id=rg.rcontentid
+                LEFT outer JOIN {rcommon_books_units} unit on unit.id=rc.unitid and unit.bookid=rc.bookid 
+                LEFT outer JOIN {rcommon_books_activities} activity on activity.id=rc.activityid and activity.bookid=rc.bookid and activity.unitid = rc.unitid                
+                WHERE rg.rcontentid = :rcontent
+                ORDER BY rg.id',
+            array('rcontent' => backup::VAR_PARENTID));
+
+        
+        $track_credentials->set_source_sql('
+                SELECT *
+                FROM {rcontent_track_credentials}
+                ORDER BY id',
+            array());
+        
+        // Define id annotations
+        $grade->annotate_ids('user', 'userid');
+        $grade_detail->annotate_ids('user', 'userid');
+
+        // Define file annotations
+        $rcontent->annotate_files('mod_rcontent', 'summary', null); // This file area hasn't itemid
+
+        // Return the root element (rcontent), wrapped into standard activity structure
+        return $this->prepare_activity_structure($rcontent);
+    }
+}

frncesc/agora-moodle-lab

mod/rcontent/backup/moodle1/lib.php
@@ -1,0 +1,188 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Provides support for the conversion of moodle1 backup to the moodle2 format
+ *
+ * @package    mod
+ * @subpackage rcontent
+ * @copyright  2012 Antoni Bertran <abertranb>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Rcontent conversion handler
+ */
+class moodle1_mod_rcontent_handler extends moodle1_mod_handler {
+
+    /** @var moodle1_file_manager */
+    protected $fileman = null;
+
+    /** @var int cmid */
+    protected $moduleid = null;
+
+    /**
+     * Declare the paths in moodle.xml we are able to convert
+     *
+     * The method returns list of {@link convert_path} instances.
+     * For each path returned, the corresponding conversion method must be
+     * defined.
+     *
+     * Note that the path /MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT does not
+     * actually exist in the file. The last element with the module name was
+     * appended by the moodle1_converter class.
+     *
+     * @return array of {@link convert_path} instances
+     */
+    public function get_paths() {
+        return array(
+            new convert_path('rcontent', '/MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT',
+                array(
+                    'renamefields' => array(
+                        'bookid' => 'isbn',
+                        'summary' => 'intro',
+                    ),
+                    'newfields' => array(
+                        'introformat' => 0
+                    )
+                )
+            ),
+            new convert_path('rcontent_grades', '/MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT/GRADES/GRADE'),
+            new convert_path('rcontent_grades_details', '/MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT/GRADES_DETAILS/GRADE_DETAIL',
+                array(
+                    'renamefields' => array(
+                        'detailid' => 'code',
+                        'detailtypeid' => 'typeid',
+                    )
+                )
+            )
+        );
+    }
+
+    /**
+     * This is executed every time we have one /MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT
+     * data available
+     */
+    public function process_rcontent($data) {
+        global $CFG;
+        // get the course module id and context id
+        $instanceid     = $data['id'];
+        $currentcminfo  = $this->get_cminfo($instanceid);
+        $this->moduleid = $currentcminfo['id'];
+        $contextid      = $this->converter->get_contextid(CONTEXT_MODULE, $this->moduleid);
+
+        // conditionally migrate to html format in summary
+        if ($CFG->texteditors !== 'textarea') {
+            $data['intro']       = text_to_html($data['intro'], false, false, true);
+        }
+
+        // get a fresh new file manager for this instance
+        $this->fileman = $this->converter->get_file_manager($contextid, 'mod_rcontent');
+
+        // convert course files embedded into the summary
+        $this->fileman->filearea = 'intro';
+        $this->fileman->itemid   = 0;
+        $data['intro'] = moodle1_converter::migrate_referenced_files($data['intro'], $this->fileman);
+
+        // check 1.9 version where backup was created
+        $backupinfo = $this->converter->get_stash('backup_info');
+        if ($backupinfo['moodle_version'] < 2007110503) {
+            // as we have no module version data, assume $currmodule->version <= $module->version
+            // - fix data as the source 1.9 build hadn't yet at time of backing up.
+            if (isset($data['grademethod'])) {
+                $data['grademethod'] = $data['grademethod']%10;
+            }
+        }
+
+        // start writing rcontent.xml
+        $this->open_xml_writer("activities/rcontent_{$this->moduleid}/rcontent.xml");
+        $this->xmlwriter->begin_tag('activity', array('id' => $instanceid, 'moduleid' => $this->moduleid,
+            'modulename' => 'rcontent', 'contextid' => $contextid));
+        $this->xmlwriter->begin_tag('rcontent', array('id' => $instanceid));
+
+        foreach ($data as $field => $value) {
+            if ($field <> 'id') {
+                $this->xmlwriter->full_tag($field, $value);
+            }
+        }
+
+        $this->xmlwriter->begin_tag('grades');
+
+        return $data;
+    }
+
+
+    /**
+     * This is executed when finish grades
+     * data available
+     */
+    /*public function on_rcontent_grades_end() {
+        $this->xmlwriter->begin_tag('grades_details');
+    }*/
+
+    /**
+     * This is executed every time we have one /MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT/GRADES/GRADE
+     * data available
+     */
+    public function process_rcontent_grades($data) {
+        $this->write_xml('grade', $data, array('/grade/id'));
+    }
+
+    /**
+    * This is executed when the parser reaches the <GRADES_DETAILS> opening element
+    */
+    public function on_rcontent_grades_details_start() {
+        $this->xmlwriter->begin_tag('grades_details');
+    }
+
+    /**
+     * This is executed when the parser reaches the closing </GRADES_DETAILS> element
+     */
+    public function on_rcontent_grades_details_end() {
+        $this->xmlwriter->end_tag('grades_details');
+    }
+    /**
+     * This is executed every time we have one /MOODLE_BACKUP/COURSE/MODULES/MOD/RCONTENT/GRADES/GRADE
+     * data available
+    */
+    public function process_rcontent_grades_details($data) {
+        $this->write_xml('grade_detail', $data, array('/grade_detail/id'));
+    }
+
+    /**
+     * This is executed when we reach the closing </MOD> tag of our 'rcontent' path
+     */
+    public function on_rcontent_end() {
+        // close rcontent.xml
+        $this->xmlwriter->end_tag('grades'); //Changed structure from rcommon->grade and  rcommon->grades_details to  rcommon->grade->grades_details
+        $this->xmlwriter->end_tag('rcontent');
+        $this->xmlwriter->end_tag('activity');
+        $this->close_xml_writer();
+
+        // write inforef.xml
+        $this->open_xml_writer("activities/rcontent_{$this->moduleid}/inforef.xml");
+        $this->xmlwriter->begin_tag('inforef');
+        $this->xmlwriter->begin_tag('fileref');
+        foreach ($this->fileman->get_fileids() as $fileid) {
+            $this->write_xml('file', array('id' => $fileid));
+        }
+        $this->xmlwriter->end_tag('fileref');
+        $this->xmlwriter->end_tag('inforef');
+        $this->close_xml_writer();
+    }
+}

mod/rcontent/backup/moodle2/backup_rcontent_stepslib.php
@@ -1,0 +1,126 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package moodlecore
+ * @subpackage backup-moodle2
+ * @copyright 2010 onwards Eloy Lafuente (stronk7) {@link http://stronk7.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Define all the backup steps that will be used by the backup_rcontent_activity_task
+ */
+
+/**
+ * Define the complete rcontent structure for backup, with file and id annotations
+ */
+class backup_rcontent_activity_structure_step extends backup_activity_structure_step {
+
+    protected function define_structure() {
+
+        // To know if we are including userinfo
+        $userinfo = $this->get_setting_value('userinfo');
+
+        // Define each element separated
+        $rcontent = new backup_nested_element('rcontent', array('id'), array(
+            'course', 'name', 'intro', 'introformat', 'levelid',
+            /*'bookid', 'unitid', 'activityid',*/ 'whatgrade',
+            'popup', 'popup_options', 'frame', 'width',
+            'height', 'timecreated', 'timemodified', 'levelcode', 'isbn', 'unitcode', 'activitycode'));
+
+        $grades = new backup_nested_element('grades');
+
+        $grade = new backup_nested_element('grade', array('id'), array(
+            'userid', 'rcontentid', /*'unitid', 'activityid',*/
+            'grade', 'mingrade', 'maxgrade', 'attempt', 'maxattempts',
+            'starttime', 'totaltime', 'maxtotaltime', 'status',
+            'comments', 'urlviewresults', 'sumweights',
+            'timecreated', 'timemodified', 'unitcode', 'activitycode'));
+
+        $grades_details = new backup_nested_element('grades_details');
+
+        $grade_detail = new backup_nested_element('grade_detail', array('id'), array(
+            'userid', 'rcontentid', /*'unitid', 'activityid',*/ 'code',
+            'typeid', 'description', 'grade', 'mingrade', 'maxgrade',
+            'starttime', 'totaltime', 'maxtotaltime', 'attempt', 'maxattempts',
+            'weight', 'urlviewresults', 'timecreated', 'timemodified', 'unitcode', 'activitycode'));
+
+        $track_credentials = new backup_nested_element('track_credentials');
+
+        $track_credential = new backup_nested_element('track_credential', array('id'), array(
+            'username', 'password', 'publisherid', 'timecreated', 'timemodified'));
+
+        // Build the tree
+        $rcontent->add_child($grades);
+        $grades->add_child($grade);
+
+        $grade->add_child($grades_details);
+        $grades_details->add_child($grade_detail);
+
+        $rcontent->add_child($track_credentials);
+        $track_credentials->add_child($track_credential);
+
+        // Define sources
+        //$rcontent->set_source_table('rcontent', array('id' => backup::VAR_ACTIVITYID));
+        $rcontent->set_source_sql('SELECT rc.*,rlevel.code as levelcode, rcb.isbn as isbn, unit.code as unitcode, activity.code as activitycode
+              FROM {rcontent} rc
+              LEFT outer JOIN {rcommon_level} rlevel on rlevel.id=rc.levelid
+              LEFT outer JOIN {rcommon_books} rcb on rcb.id=rc.bookid and rcb.levelid=rc.levelid
+              LEFT outer JOIN {rcommon_books_units} unit on unit.id=rc.unitid and unit.bookid=rc.bookid
+              LEFT outer JOIN {rcommon_books_activities} activity on activity.id=rc.activityid and activity.bookid=rc.bookid and activity.unitid = rc.unitid
+             WHERE rc.id = ?', array(backup::VAR_ACTIVITYID));
+
+        // Use set_source_sql for other calls as set_source_table returns records in reverse order
+        // and order is important for several rcontent fields - esp rcontent_scoes.
+        $grade->set_source_sql('
+                SELECT rg.*, unit.code as unitcode, activity.code as activitycode
+                FROM {rcontent_grades} rg
+                INNER JOIN {rcontent} rc ON rc.id=rg.rcontentid
+                LEFT outer JOIN {rcommon_books_units} unit on unit.id=rc.unitid and unit.bookid=rc.bookid
+                LEFT outer JOIN {rcommon_books_activities} activity on activity.id=rc.activityid and activity.bookid=rc.bookid and activity.unitid = rc.unitid
+                WHERE rg.rcontentid = :rcontent
+                ORDER BY rg.id',
+            array('rcontent' => backup::VAR_PARENTID));
+
+        $grade_detail->set_source_sql('
+                SELECT rc.*, unit.code as unitcode, activity.code as activitycode
+                FROM {rcontent_grades_details} rg
+                INNER JOIN {rcontent} rc ON rc.id=rg.rcontentid
+                LEFT outer JOIN {rcommon_books_units} unit on unit.id=rc.unitid and unit.bookid=rc.bookid
+                LEFT outer JOIN {rcommon_books_activities} activity on activity.id=rc.activityid and activity.bookid=rc.bookid and activity.unitid = rc.unitid
+                WHERE rg.rcontentid = :rcontent
+                ORDER BY rg.id',
+            array('rcontent' => backup::VAR_PARENTID));
+
+
+        $track_credentials->set_source_sql('
+                SELECT *
+                FROM {rcontent_track_credentials}
+                ORDER BY id',
+            array());
+
+        // Define id annotations
+        $grade->annotate_ids('user', 'userid');
+        $grade_detail->annotate_ids('user', 'userid');
+
+        // Define file annotations
+        $rcontent->annotate_files('mod_rcontent', 'intro', null); // This file area hasn't itemid
+
+        // Return the root element (rcontent), wrapped into standard activity structure
+        return $this->prepare_activity_structure($rcontent);
+    }
+}